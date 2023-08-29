303c3790dd8b3f8de2b2028d2cdadcefca3f7478
Shubham Nagpure <josh.shubham@dt.ae>
2023-08-29 16:40:46 +0530

    Updated dashboard layout

--- src/SharedComponents/Header/index.tsx
+++ src/SharedComponents/Header/index.tsx
@@ -1,16 +1,49 @@
-import { Menu, Layout } from 'antd';
+import { useState } from 'react';
+import { Layout, Popover } from 'antd';
+
+import PopOverContent from '../PopOverContent/index';
+import i18Icon from '../../assets/images/i18.svg';
+import './index.scss';
 
 const { Header } = Layout;
 
 const HeaderComponent: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const hide = () => {
+    setOpen(false);
+  };
+
+  const handleOpenChange = (newOpen: boolean) => {
+    setOpen(newOpen);
+  };
+
   return (
-    <Header style={{ display: 'flex', alignItems: 'right' }}>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        style={{ display: 'flex', alignItems: 'right' }}
-        defaultSelectedKeys={['2']}
-      />
+    <Header className="header">
+      <div className="demo-logo">LOGO</div>
+      <div className="menu">
+        <div className="i18-button">
+          <Popover
+            content={PopOverContent(hide)}
+            overlayStyle={{
+              width: '139px',
+              height: '70px'
+            }}
+            trigger="click"
+            open={open}
+            onOpenChange={handleOpenChange}
+          >
+            <img className="i18-icon" src={i18Icon} />
+          </Popover>
+        </div>
+        <div>
+          <img
+            src="https://picsum.photos/id/237/200/300"
+            alt=""
+            className="image-avatar"
+          />
+        </div>
+      </div>
     </Header>
   );
 };
