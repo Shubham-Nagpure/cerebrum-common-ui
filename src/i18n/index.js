503c4fcfd549adf52dfffef2f0d7f609eda30514
Malhar Jadhav <malharjadhav8999@gmail.com>
2023-08-29 12:42:48 +0530

    i18nNext configuration added

---
+++ src/i18n/index.js
@@ -0,0 +1,27 @@
+/**
+  @author - Malhar Jadhav
+  @description - Internationalization (i18n) configuration setup
+*/
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enTranslation from './translations/en.json';
+import arTranslation from './translations/ar.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: enTranslation },
+    ar: { translation: arTranslation }
+    // Add more languages as needed
+  },
+  fallbackLng: 'en', // default fallback language as english
+  debug: true, // to show debugging messsages onto the console
+  // interpolation used to define how dynamic values to be inserted into translated strings
+  // should be handled
+  interpolation: {
+    escapeValue: false
+  }
+});
+
+export default i18n;
